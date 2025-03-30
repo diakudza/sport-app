4fbaad81902ca3349a23fc7953c9367d8e733eda
Mike Dyakov <m.dyakov@zaryad.tech>
2025-03-30 23:33:11 +0300

    билд

--- resources/views/sportsman/your-training.blade.php
+++ resources/views/sportsman/your-training.blade.php
@@ -6,34 +6,33 @@
     <div class="flex flex-col">
 
 
-    <h1 class="text-2xl font-bold mb-4">Ваши тренировки</h1>
-    <table class="w-auto bg-white shadow-lg rounded-lg overflow-hidden">
-        <thead class="bg-blue-600 text-white">
-        <th class="p-2">Тип</th>
-        <th class="p-2">Сумма</th>
-        <th class="p-2">Дата добавления</th>
-        <th class="p-2">Событие</th>
-        <th class="p-2"></th>
-        <th class="p-2">Проверена</th>
-        </thead>
-        <tbody>
-        @foreach($trainings as $training)
-            <tr class="border-b cursor-pointer"
-                onclick="window.location.href = '{{ route('training.detail', $training->id) }}'">
-                <td class="p-2">{{$training->type?->name}}</td>
-                <td class="p-2 ">{{$training->points}}</td>
-                <td class="p-2 ">{{$training->created_at}}</td>
-                <td class="p-2 ">{{$training->event?->name}}</td>
-                <td class="p-2 ">@if($training->attachments->count())
-
+        <h1 class="text-2xl font-bold mb-4">Ваши тренировки</h1>
+        <table class="w-auto bg-white shadow-lg rounded-lg overflow-hidden">
+            <thead class="bg-blue-600 text-white">
+            <th class="p-2">Тип</th>
+            <th class="p-2">Сумма</th>
+            <th class="p-2">Дата добавления</th>
+            <th class="p-2">Событие</th>
+            <th class="p-2"></th>
+            <th class="p-2">Проверена</th>
+            </thead>
+            <tbody>
+            @foreach($trainings as $training)
+                <tr class="border-b cursor-pointer"
+                    onclick="window.location.href = '{{ route('training.detail', $training->id) }}'">
+                    <td class="p-2">{{$training->type?->name}}</td>
+                    <td class="p-2 ">{{$training->points}}</td>
+                    <td class="p-2 ">{{$training->created_at}}</td>
+                    <td class="p-2 ">{{$training->event?->name}}</td>
+                    <td class="p-2 ">
+                        @if($training->attachments->count())
                             <img width="100" src="/storage/{{$training->attachments?->first()?->path}}"/>
-
-                    @endif
-                </td>
-                <td class="p-2 ">{{$training->approved ? 'Да' : 'Нет'}}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+                        @endif
+                    </td>
+                    <td class="p-2 ">{{$training->approved ? 'Да' : 'Нет'}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
